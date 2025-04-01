2c871081009fab7d07e76a06e8cdae56f450deab
Andrei Lysov <alysov@demonware.net>
2025-04-01 14:30:58 +0100

    rewrite mysql ops to transactions and connections

--- src/aimsir/model/mysql.rs
+++ src/aimsir/model/mysql.rs
@@ -27,87 +27,109 @@ impl MysqlDb {
 #[async_trait]
 impl model::db::Db for MysqlDb {
     async fn get_peers(&mut self) -> Result<Vec<model::Peer>, sqlx::Error> {
+        let mut conn = self.conn.acquire().await?;
         let results = sqlx::query_as!(model::Peer, "SELECT * FROM peers",)
-            .fetch_all(&self.conn)
+            .fetch_all(&mut *conn)
             .await?;
         Ok(results)
     }
     async fn get_tags(&mut self) -> Result<Vec<model::Tag>, sqlx::Error> {
+        let mut conn = self.conn.acquire().await?;
         let results = sqlx::query_as!(model::Tag, "SELECT * FROM tags",)
-            .fetch_all(&self.conn)
+            // .fetch_all(&self.conn)
+            .fetch_all(&mut *conn)
             .await?;
         Ok(results)
     }
     async fn add_tag(&mut self, tag: model::Tag) -> Result<(), sqlx::Error> {
+        let mut conn = self.conn.begin().await?;
         let max_id_request = sqlx::query!("SELECT IFNULL(MAX(id), -1) as id FROM tags",)
-            .fetch_one(&self.conn)
+            // .fetch_one(&self.conn)
+            .fetch_all(&mut *conn)
             .await?;
-        let next_id = max_id_request.id + 1;
+        let next_id = max_id_request[0].id + 1;
         let _results = sqlx::query!(
             "INSERT INTO tags VALUES (?, ?, ?)",
             next_id,
             tag.parent,
             tag.name,
         )
-        .execute(&self.conn)
+        .execute(&mut *conn)
+        // .execute(&self.conn)
         .await?;
+        conn.commit().await?;
         Ok(())
     }
     async fn del_tag(&mut self, tag_id: i32) -> Result<(), sqlx::Error> {
+        let mut conn = self.conn.begin().await?;
         let tag = sqlx::query_as!(model::Tag, "SELECT * FROM tags WHERE id = ?", tag_id,)
-            .fetch_one(&self.conn)
+            .fetch_one(&mut *conn)
             .await?;
         let _results = sqlx::query!(
             "UPDATE tags SET parent = ? WHERE parent = ?",
             tag.parent,
             tag_id
         )
-        .execute(&self.conn)
+        .execute(&mut *conn)
+        // .execute(&self.conn)
         .await?;
         let _results = sqlx::query!("DELETE FROM tags WHERE id = ?", tag_id,)
-            .execute(&self.conn)
+            .execute(&mut *conn)
+            // .execute(&self.conn)
             .await?;
         let _results = sqlx::query!("DELETE FROM peer_tags WHERE tag_id = ?", tag_id,)
-            .execute(&self.conn)
+            .execute(&mut *conn)
+            // .execute(&self.conn)
             .await?;
+        conn.commit().await?;
         Ok(())
     }
     async fn add_peer_tag(&mut self, peer_tag: model::PeerTag) -> Result<(), sqlx::Error> {
+        let mut conn = self.conn.begin().await?;
         let _results = sqlx::query!(
             "INSERT INTO peer_tags VALUES (?, ?)",
             peer_tag.peer_id,
             peer_tag.tag_id,
         )
-        .execute(&self.conn)
+        .execute(&mut *conn)
+        // .execute(&self.conn)
         .await?;
+        conn.commit().await?;
         Ok(())
     }
     async fn del_peer_tag(&mut self, peer_id: String, tag_id: i32) -> Result<(), sqlx::Error> {
+        let mut conn = self.conn.begin().await?;
         let _results = sqlx::query!(
             "DELETE FROM peer_tags WHERE peer_id = ? AND tag_id = ?",
             peer_id,
             tag_id,
         )
-        .execute(&self.conn)
+        .execute(&mut *conn)
         .await?;
+        conn.commit().await?;
         Ok(())
     }
     async fn get_peer_tags(&mut self) -> Result<Vec<model::PeerTag>, sqlx::Error> {
+        let mut conn = self.conn.acquire().await?;
         let results = sqlx::query_as!(model::PeerTag, "SELECT * FROM peer_tags",)
-            .fetch_all(&self.conn)
+            .fetch_all(&mut *conn)
             .await?;
         Ok(results)
     }
     async fn add_peer(&mut self, peer: model::Peer) -> Result<(), sqlx::Error> {
+        let mut conn = self.conn.begin().await?;
         let _results = sqlx::query!("INSERT INTO peers VALUES (?, ?)", peer.peer_id, peer.name)
-            .execute(&self.conn)
+            .execute(&mut *conn)
             .await?;
+        conn.commit().await?;
         Ok(())
     }
     async fn del_peer(&mut self, peer_id: String) -> Result<(), sqlx::Error> {
+        let mut conn = self.conn.begin().await?;
         let _results = sqlx::query!("DELETE FROM peers WHERE peer_id = ?", peer_id,)
-            .execute(&self.conn)
+            .execute(&mut *conn)
             .await?;
+        conn.commit().await?;
         Ok(())
     }
 }
